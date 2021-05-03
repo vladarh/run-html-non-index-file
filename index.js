da3ef72aaf352c900dc61b1edcb3506aac579098
unknown <vladarh11v@gmail.com>
2021-05-03 22:18:29 +0300

    d

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/test.html'));
 });
-
+var port_number = server.listen(process.env.PORT || 3000);
 app.listen(3000, () => {
   console.log('server started');
 });

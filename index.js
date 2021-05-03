1ced0d14b1307cde8c85325f8a51eed0abf4eeb5
Rubcov V <>
2021-05-03 17:31:17 +0000

    g

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static(__dirname))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/foo.html'));
+  res.sendFile(path.join(__dirname + '/test.html'));
 });
 
 app.listen(3000, () => {

13b003c30e127d5c5da7fa4d864c5cbc33a923d1
unknown <vladarh11v@gmail.com>
2021-05-03 22:56:37 +0300

    d

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/test.html'));
 });
 
-var port_number = index.listen(process.env.PORT || 3000);
+var port_number = server.listen(process.env.PORT || 3000);
 app.listen(port_number);
